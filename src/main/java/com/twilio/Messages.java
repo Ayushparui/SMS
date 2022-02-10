182928a13e0f8f639a3678628c17ac5fa444c5bb
Ayush Parui <paruiayush900@gmail.com>
2022-02-10 08:56:48 +0530

    Update Messages.java
    Account SID ,Account token and phone numbers has been removed

--- src/main/java/com/twilio/Messages.java
+++ src/main/java/com/twilio/Messages.java
@@ -12,14 +12,14 @@ import com.twilio.type.PhoneNumber;
 
 public class Messages {
 
-    public static final String Account_SID = "AC89eeb06b612e96dd52cf99f6dace3482";
-    public static final String AUTH_TOKEN = "aa260ce52e1b6e7d148914ab019a2fd4";
+    public static final String Account_SID = "AC89xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    public static final String AUTH_TOKEN = "aa260xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
     public static void main(String[] args) {
     Twilio.init(Account_SID, AUTH_TOKEN);
     Message message = Message.creator(
-            new PhoneNumber("+918657476630"),
-            new PhoneNumber("+19377875716"),
+            new PhoneNumber("+91xxxxxxxxxx"),
+            new PhoneNumber("+1xxxxxxxxx"),
             "Hello How are you I'm using java"
     ).create();
         System.out.println(message.getSid());
